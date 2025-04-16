b904fab89d6a77cf087c0fd187e53406f46eeefb
Baskaran Ragikaran <125803055+Ragikaran2003@users.noreply.github.com>
2025-04-16 22:51:50 +0530

    Update main.jsx
    same

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import { QrCode } from './QrCode'
-import './Qrcode.css'
+import './QrCode.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Qrcode />
+    <QrCode />
   </StrictMode>,
 )
