42ea13fd6aecdab356d80f2745eb0a88b7768bfb
Baskaran Ragikaran <125803055+Ragikaran2003@users.noreply.github.com>
2025-04-16 22:46:39 +0530

    Update QrCode.jsx
    same

--- src/QrCode.jsx
+++ src/QrCode.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const Qrcode = () => {
+export const QrCode = () => {
   const [img , setImg] = useState("");
   const [loading , setLoading] = useState(false);
   const [qrData,setQrData] = useState("")
